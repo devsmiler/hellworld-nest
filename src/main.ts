d7a37da334ae5521fee806500347813c00229fea
Min Kyu Kim <devsmile@twc.co.kr>
2021-07-26 18:31:29 +0900

    first api by nest js

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);//nestFactory를 호출한다.
+  await app.listen(8000);// 그리고 3000포트를 리슨 중입니다.
 }
 bootstrap();
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Param, Delete, Put, Patch, Body } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+    
+    constructor(private readonly movieService: MoviesService){}
+    
+    @Get()
+    getAll(): Movie[]{
+        return this.movieService.getAll();
+    }
+    
+    @Get("/:id")//get id 파라미터와 @Param id 파라미터는 같아야 합니다.
+    getOne(@Param("id") movieId: string): Movie{// id 와 함수 내의 로컬 변수 id는 같아야합니다
+        return this.movieService.getOne(movieId);
+    }
+    
+    @Post()//get id 파라미터와 @Param id 파라미터는 같아야 합니다.
+    create(@Body() movieData){// id 와 함수 내의 로컬 변수 id는 같아야합니다
+        return this.movieService.create(movieData);
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId: string){
+        return this.movieService.deleteOne(movieId);
+    }
+    
+    @Patch('/:id')
+    patch(@Param('id') movieId: string){
+        return `patch a movie id : ${movieId}`;
+    }
+    
+}
